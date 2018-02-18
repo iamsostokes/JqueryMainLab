09256e80f7cfbf6f4589fb731774e532b1e1b7c1
Ja'mez Stokes <32825749+iamsostokes@users.noreply.github.com>
2018-02-17 19:30:01 -0500

    part two changes

--- jquerylab1.js
+++ jquerylab1.js
@@ -1,15 +1,30 @@
-// $('.table').hover(function(){
-//   $('this').css("background-color", "red");
-// });
+$('#reservation-form').hide();
 
-
-// $('.table').on("mouseover", function(){
-//   $('this').css("background-color", "red");
-// });
-
-
-$('.reservationForm').hide();
-
-$('table').click (function(){
+$('.table').on('click',function(){
+  var number = $(this).find('.tableNum').text();
+  $('#reservation-form h2').text('Table Number: ' + number);
+  $('#reservation-form').show();
+  
+  $('table').addClass(reserved);
   
 });
+
+$('.close').on('click', function(){
+  $('#reservation-form').hide();
+});
+
+
+var customer_reservation = [];
+$('#subbtn').on('click', function(){
+  var input1 = $('#one');
+  var input2 = $('#two');
+  var input3 = $('#three');
+  var reserved = {
+    name: input1.val(),
+    number: input2.val(),
+    size: input3.val()
+  };
+  customer_reservation.push(reservation-form);
+  $('#reservation-form').hide();
+});
+  
